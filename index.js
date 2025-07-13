66c2128d449da7372e5e146cebb4d3d4228c6fb9
Roy Koigu <roykoigu@protonmail.com>
2025-07-13 12:18:43 +0300

    completed code challenge

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const overExaggeratedAlert = () => alert("Hey I was pressed!")
+
 function addingEventListener() {
+    const input = document.getElementById('button')
+    input.addEventListener("click",overExaggeratedAlert);
 }
+addingEventListener()
+
+
+
+
